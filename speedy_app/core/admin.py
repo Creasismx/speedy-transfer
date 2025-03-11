102ad44a48a20125418d57b45e4dfadf590f25bc
Antonio Tesillos <tesillosguns@gmail.com>
2025-03-11 17:00:18 -0600

    Limitar destinos solo a Vallarta y Barra de Navidad

--- speedy_app/core/admin.py
+++ speedy_app/core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Zone, Hotel, Car, Rate
+from .models import Zone, Hotel, Car, Rate, Reservation, Payment
 
 admin.site.register(Zone)
 admin.site.register(Hotel)
 admin.site.register(Car)
 admin.site.register(Rate)
+admin.site.register(Reservation)
+admin.site.register(Payment)
+
--- speedy_app/core/views.py
+++ speedy_app/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
+from .models import Zone
 
 class LandingView(TemplateView):
     template_name = "speedy_app/landing_page.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['zones'] = Zone.objects.all()
+        return context
