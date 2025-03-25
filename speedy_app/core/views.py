22cdec1bbd54e8cc9781ad98ee54ad7bdd382ab4
Antonio Tesillos <tesillosguns@gmail.com>
2025-03-24 20:59:55 -0600

    Finalizar catálogo de unidades - Vehiculos

--- speedy_app/core/views.py
+++ speedy_app/core/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic import TemplateView
 from django.http import HttpResponse
-from .models import Zone, Contact
+from .models import Zone, Contact, Car
 
 class LandingView(TemplateView):
     template_name = "speedy_app/landing_page.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['zones'] = Zone.objects.all()
+        context['cars'] = Car.objects.all()
         return context
     
 
