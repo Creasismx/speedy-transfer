028a5f3e75132fb9ec8eee2c1cf3ff5b4aeb793c
adolfo1971 <adolfomariscalh@hotmail.com>
2025-11-10 18:34:21 -0600

    Setup Django before websocket routing import
    Added Django setup before importing websocket routing.

--- config/asgi.py
+++ config/asgi.py
@@ -3,10 +3,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
+# ✅ Django setup BEFORE importing websocket routing
+import django
+django.setup()
 
-from chat.routing import websocket_urlpatterns  # import AFTER settings module defined
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
