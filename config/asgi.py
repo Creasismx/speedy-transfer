76bfcd8a09477c5f6d5c8a27bbac17c9ee68798c
adolfo1971 <adolfomariscalh@hotmail.com>
2025-11-10 18:30:10 -0600

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,33 +1,16 @@
 import os
-import logging
 from django.core.asgi import get_asgi_application
-
-# Configure logging
-logger = logging.getLogger(__name__)
-
-# Set Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
-
-# Get Django ASGI application first
-django_asgi_app = get_asgi_application()
-
-# Import Django and set it up
-import django
-django.setup()
-
-# Import other dependencies after Django is set up
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-# Import websocket patterns last
-from chat.routing import websocket_urlpatterns
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-# Create application
+
+from chat.routing import websocket_urlpatterns  # import AFTER settings module defined
+
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
